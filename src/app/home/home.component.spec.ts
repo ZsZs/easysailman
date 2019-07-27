3c222a093b189599e2b250690a747668a60e48bb
ZsZs <zsolt.zsuffa@gmail.com>
2019-07-27 07:41:37 +0200

    Skip tests for now.

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './wellcome.component';
+import { HomeComponent } from './home.component';
 
 describe('WellcomeComponent', () => {
   let component: HomeComponent;
